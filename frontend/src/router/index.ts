aecff523affc2d4ba7d722d433d501fd8308cded
LeahFu <leahfu818@gmail.com>
2023-05-12 21:00:29 -0400

    install nprogress

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 //1. import Vue Router module
 import { createRouter, createWebHashHistory } from "vue-router"
+import Nprogress from "../config/nprogress.ts"
 //2. Define some routes, each route needs to be mapped to a component
 const routes = [{
     path:'/',
@@ -11,6 +12,24 @@ const routes = [{
 const router = createRouter({
     history: createWebHashHistory(),
     routes:routes
+})
+
+//Route interception guard
+router.beforeEach(async(to,from,next)=>{
+    //1. start Nprogress
+    Nprogress.start()
+    next()
+})
+
+// Routing jump ends
+router.afterEach(()=>{
+    Nprogress.done()
+})
+
+// Routing jump fail
+router.onError(error=>{
+    Nprogress.done()
+    console.warn("Routing error",error.message)
 })
 
 export default router
