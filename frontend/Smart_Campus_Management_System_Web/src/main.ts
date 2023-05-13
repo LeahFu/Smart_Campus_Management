909d70f5907b5efe6a03332d2a9ba7cfaaea3098
LeahFu <leahfu818@gmail.com>
2023-05-13 14:25:20 -0400

    install element plus

--- frontend/Smart_Campus_Management_System_Web/src/main.ts
+++ frontend/Smart_Campus_Management_System_Web/src/main.ts
@@ -2,7 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router";
+import pinia from "./store";
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+//Import all element-plus icon
+import * as ElementPlusIconsVue from "@element-plus/icons-vue"
+
 const app = createApp(App)
 app.use(router)
-//app.use(pinia)
+app.use(pinia)
+//Register all icons globally
+for(const [key, component] of Object.entries(ElementPlusIconsVue)){
+    app.component(key, component)
+}
+app.use(ElementPlus)
 app.mount('#app')
