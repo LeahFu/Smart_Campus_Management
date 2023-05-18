932c33ce8575f974e6f56a4068ef6e2c50b0322b
LeahFu <leahfu818@gmail.com>
2023-05-18 10:37:32 -0400

    Login Page Layout-1

---
+++ frontend/Smart_Campus_Management_System_Web/src/views/login/Login.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="login-container">
+        <div class="login-box">
+            <!--left background start-->
+            <div class="login-left">
+                <img src="../../assets/login/side-logo.png">
+            </div>
+            <!--left background end-->
+            <!--login form start-->
+            <div class="login-form">
+                <!--title and logo start-->
+                <div class="login-title">
+                    <img class="icon" src="../../assets/logo2.png">
+                    <h2 class="title">Smart Campus Management System</h2>
+                </div>
+                <!--title and logo end-->
+                <LoginForm/>
+            </div>
+            <!--login form end-->
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/Smart_Campus_Management_System_Web/src/views/login/components/LoginForm.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <el-form
+            ref="ruleFormRef"
+            :model="ruleForm"
+            :rules="rules"
+    >
+        <el-form-item label="" prop="username">
+            <el-input placeholder="Please enter username" autoComplete="on" style="position: relative" v-model="ruleForm.username">
+                <template #prefix>
+                    <el-icon class="el-input__icon"><UserFilled /></el-icon>
+                </template>
+            </el-input>
+        </el-form-item>
+
+        <el-form-item label="" prop="password">
+            <el-input
+                    placeholder="Please enter password"
+                    autoComplete="on"
+                    v-model="ruleForm.password"
+                    :type="passwordType"
+            >
+                <template #prefix>
+                    <el-icon class="el-input__icon"><GoodsFilled /></el-icon>
+                </template>
+                <template #suffix>
+                    <div class="show-pwd" @click="showPwd">
+                        <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"/>
+                    </div>
+                </template>
+            </el-input>
+        </el-form-item>
+
+        <el-form-item style="width: 100%">
+            <el-button
+                    :loading="loading"
+                    class="login-btn"
+                    type="success"
+                    @click="submitForm(ruleFormRef)"
+            >Login</el-button
+            >
+        </el-form-item>
+
+    </el-form>
+</template>
+
+<style scoped>
+
+</style>
