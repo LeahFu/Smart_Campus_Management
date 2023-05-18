463ace2148b47b117272957bbf20255951dd3474
LeahFu <leahfu818@gmail.com>
2023-05-18 16:21:50 -0400

    create entity class SysUser

---
+++ backend/Smart_Campus_Management_System/src/main/java/ca/access/student/domain/SysUser.java
@@ -0,0 +1,34 @@
+package ca.access.student.domain;
+
+import ca.access.base.BaseEntity;
+import ca.access.base.BaseResult;
+import jakarta.persistence.*;
+import lombok.Data;
+
+/**
+ * @author: Lei Fu
+ * @date: 2023/05/18
+ * @description: User entity class
+ */
+@Data
+@Entity
+@Table(name = "sys_user")
+@org.hibernate.annotations.Table(appliesTo = "sys_user",comment = "user information table")
+public class SysUser extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "realname")
+    private String realname;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "status")
+    private Integer status;
+
+
+}
