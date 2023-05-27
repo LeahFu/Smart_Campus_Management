b3f015f7d84994adbc9f49bcbba49525d2fe6947
LeahFu <leahfu818@gmail.com>
2023-05-27 13:07:28 -0400

    Layout of the page for adding role

---
+++ frontend/Smart_Campus_Management_System_Web/src/views/role/components/AddRole.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {ref, reactive} from 'vue'
+
+const subLoading = ref(false)
+const formRole = reactive({
+    name: '',
+    code: '',
+    remarks: ''
+})
+</script>
+
+<template>
+    <el-form :model="formRole"  label-width="80px">
+        <el-row>
+            <el-col :span="12">
+                <el-form-item label="role name" prop="name">
+                    <el-input v-model="formRole.name" placeholder="Please enter role name" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="12">
+                <el-form-item label="role code" prop="code">
+                    <el-input v-model="formRole.code" placeholder="Please enter role code" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="24">
+                <el-form-item label="remark">
+                    <el-input
+                            v-model="formRole.remarks"
+                            :rows="2"
+                            type="textarea"
+                            placeholder="Please enter a remark"
+                    />
+                </el-form-item>
+            </el-col>
+        </el-row>
+    </el-form>
+
+    <div class="dialong__button--wrap">
+        <el-button @click="close">Cancel</el-button>
+        <el-button color="#178557" :loading="subLoading" type="success" @click="addRole(ruleFormRef)">Save</el-button>
+    </div>
+</template>
+
+<style scoped>
+.dialong__button--wrap {
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
