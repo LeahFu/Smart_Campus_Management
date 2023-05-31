b2c179cf2cc10fcf004c93851dadc04f10ef45df
LeahFu <leahfu818@gmail.com>
2023-05-31 09:17:36 -0400

    add class - page layout

---
+++ frontend/Smart_Campus_Management_System_Web/src/views/gradeclass/components/AddGradeClass.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <el-form  :model="formGradeClass"  label-width="80px">
+        <el-row>
+            <el-col :span="12">
+                <el-form-item label="class code" prop="code">
+                    <el-input v-model="formGradeClass.code" placeholder="Please enter class code" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="12">
+                <el-form-item label="class name" prop="name">
+                    <el-input v-model="formGradeClass.name" placeholder="Please enter class name" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="12">
+                <el-form-item label="grade" prop="grade">
+                    <el-input v-model="formGradeClass.grade" placeholder="Please enter grade" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="12">
+                <el-form-item label="class" prop="clazz">
+                    <el-input v-model="formGradeClass.clazz" placeholder="Please enter class" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="24">
+                <el-form-item label="remarks">
+                    <el-input
+                            v-model="formGradeClass.remarks"
+                            :rows="2"
+                            type="textarea"
+                            placeholder="Please enter remarks"
+                    />
+                </el-form-item>
+            </el-col>
+        </el-row>
+    </el-form>
+
+    <div class="dialong__button--wrap">
+        <el-button @click="close">Cancel</el-button>
+        <el-button color="#178557" :loading="subLoading" type="success" @click="addGradeClass">Save</el-button>
+    </div>
+</template>
+
+<style scoped>
+.dialong__button--wrap {
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
