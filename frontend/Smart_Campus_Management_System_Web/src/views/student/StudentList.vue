32bb3cf21f66133b886d8660a8723ec71bd4b257
LeahFu <leahfu818@gmail.com>
2023-06-02 14:31:24 -0400

    student management-table box layout

--- frontend/Smart_Campus_Management_System_Web/src/views/student/StudentList.vue
+++ frontend/Smart_Campus_Management_System_Web/src/views/student/StudentList.vue
@@ -28,6 +28,71 @@
                     </el-row>
                 </div>
                 <!--Search area end-->
+            </div>
+        </template>
+        <!--Header end-->
+        <!--Table-box start-->
+        <div class="table-box">
+            <el-table element-loading-text="Loading..." v-loading="loading" :data="tableData"
+                      style="width: 100%;text-align: center" :cell-style="{textAlign: 'center'}"
+                      :row-class-name="rowClassName"
+                      :header-cell-style="{fontSize: '15px', background: '#178557',color: 'white',textAlign: 'center'}">
+
+                <el-table-column label="Serial number" width="100" type="index" :index="Nindex"/>
+                <el-table-column label="Student number">
+                    <template #default="scope">
+                        <span>{{scope.row.stuno}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Name">
+                    <template #default="scope">
+                        <span>{{scope.row.name}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Grade">
+                    <template #default="scope">
+                        <span>{{scope.row.gradeClass.code}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Gender">
+                    <template #default="scope">
+                        <span>{{scope.row.sex}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Phone">
+                    <template #default="scope">
+                        <span>{{scope.row.phone}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Email">
+                    <template #default="scope">
+                        <span>{{scope.row.qq}}</span>
+                    </template>
+                </el-table-column>
+
+
+                <el-table-column label="Operate">
+                    <template #default="scope">
+                        <el-button size="small" @click="editStudent(scope.row.id)"
+                                   style="margin: 0 0 10px 10px;">Edit</el-button>
+                        <el-popconfirm confirm-button-text="Submit" cancel-button-text="Cancel" :icon="Delete"
+                                       icon-color="#626AEF" :title="'Are you sure to delete student “'+scope.row.name+'” ？'"
+                                       @confirm="delStudent(scope.row.id)">
+                            <template #reference>
+                                <el-button size="small" type="danger" style="margin-bottom: 10px;">Delete</el-button>
+                            </template>
+                        </el-popconfirm>
+                    </template>
+                </el-table-column>
+
+            </el-table>
+        </div>
+        <!--Table-box  end-->
 </template>
 
 <style scoped>
