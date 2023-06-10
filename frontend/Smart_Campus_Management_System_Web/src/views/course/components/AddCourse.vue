6955152080f32616d3927f576be202277f0a0648
LeahFu <leahfu818@gmail.com>
2023-06-10 17:58:53 -0400

    implement function - check（add course form）

--- frontend/Smart_Campus_Management_System_Web/src/views/course/components/AddCourse.vue
+++ frontend/Smart_Campus_Management_System_Web/src/views/course/components/AddCourse.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {ref, reactive} from 'vue'
 import {ElMessage} from 'element-plus'
+import type { FormInstance, FormRules } from 'element-plus'
 import addCourse from "./AddCourse.vue";
 
 
@@ -13,34 +14,49 @@ const formCourse = reactive({
     remarks: ''
 })
 // Add course information
-const addCourse = async () => {
+const addCourse = async (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    await formEl.validate(async (valid, fields) => {
         subLoading.value = true
-            const { data } =  await addCourseApi(formCourse)
-            if(data.status===200){
+        if (valid) {
+            const {data} = await addCourseApi(formCourse)
+            if (data.status === 200) {
                 ElMessage.success(data.message)
                 emit('success')
-            }else {
+            } else {
                 ElMessage.error(data.message)
             }
+        } else {
+            ElMessage.error('Submission failed, you still have unfilled items!')
+            console.log('error submit!', fields)
+        }
         subLoading.value = false
+    })
 }
 const emit = defineEmits(['closeAddCourseForm','success'])
 // Cancel form
 const close = ()=> {
     emit('closeAddCourseForm')
 }
+// Form instance object
+const ruleFormRef = ref<FormInstance>()
+// Define Form Constraint Rule Objects
+const rules = reactive<FormRules>({
+    courseno: [{ required: true, message: 'Course number cannot be empty', trigger: 'blur' }],
+    coursename: [{ required: true, message: 'Course name cannot be empty', trigger: 'blur' }]
+})
 </script>
 
 <template>
-    <el-form  :model="formCourse"  label-width="80px">
+    <el-form ref="ruleFormRef" :rules="rules" :model="formCourse"  label-width="80px">
         <el-row>
             <el-col :span="12">
-                <el-form-item label="Course NO.">
+                <el-form-item label="Course number" prop="courseno">
                     <el-input v-model="formCourse.courseno" placeholder="Please enter course NO." />
                 </el-form-item>
             </el-col>
             <el-col :span="12">
-                <el-form-item label="Course name">
+                <el-form-item label="Course name" prop="coursename">
                     <el-input v-model="formCourse.coursename" placeholder="Please enter course name" />
                 </el-form-item>
             </el-col>
@@ -60,7 +76,7 @@ const close = ()=> {
 
     <div class="dialong__button--wrap">
         <el-button @click="close">Cancel</el-button>
-        <el-button color="#178557" :loading="subLoading" type="success" @click="addCourse">Save</el-button>
+        <el-button color="#178557" :loading="subLoading" type="success" @click="addCourse(ruleFormRef)">Save</el-button>
     </div>
 </template>
 
