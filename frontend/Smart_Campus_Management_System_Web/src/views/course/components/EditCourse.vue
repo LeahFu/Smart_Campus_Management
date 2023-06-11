5638c6e4ae2f09941de62fb5b534d68f54b36d55
LeahFu <leahfu818@gmail.com>
2023-06-11 12:14:40 -0400

    update course - page layout

---
+++ frontend/Smart_Campus_Management_System_Web/src/views/course/components/EditCourse.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <el-form :model="formCourse"  label-width="80px">
+        <el-row>
+            <el-col :span="12">
+                <el-form-item label="Course number" prop="courseno">
+                    <el-input v-model="formCourse.courseno" placeholder="Please enter course number" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="12">
+                <el-form-item label="Course name" prop="coursename">
+                    <el-input v-model="formCourse.coursename" placeholder="Please enter course name" />
+                </el-form-item>
+            </el-col>
+
+            <el-col :span="24">
+                <el-form-item label="Remarks">
+                    <el-input
+                            v-model="formCourse.remarks"
+                            :rows="2"
+                            type="textarea"
+                            placeholder="Please enter remarks"
+                    />
+                </el-form-item>
+            </el-col>
+        </el-row>
+    </el-form>
+
+    <div class="dialong__button--wrap">
+        <el-button @click="close">Cancel</el-button>
+        <el-button color="#178557" :loading="subLoading" type="success" @click="editCourse(ruleFormRef)">Save</el-button>
+    </div>
+</template>
+
+<style scoped>
+.dialong__button--wrap {
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
