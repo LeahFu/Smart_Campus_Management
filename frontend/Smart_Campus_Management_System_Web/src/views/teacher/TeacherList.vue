eef989271bad588465479cd50e87cf065b3be8ba
LeahFu <leahfu818@gmail.com>
2023-06-12 11:57:10 -0400

    teacher management page - header layout

---
+++ frontend/Smart_Campus_Management_System_Web/src/views/teacher/TeacherList.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <el-card class="box-card">
+        <!--Header start-->
+        <template #header>
+            <div class="card-header">
+                <h3>
+                    <el-icon style="margin-right: 10px;"><UserFilled /></el-icon>Teacher Management
+                </h3>
+
+                <!--Search area start-->
+                <div class="card-search">
+                    <el-row :gutter="10">
+                        <el-col :span="10">
+                            <el-input :prefix-icon="Search" v-model="searchValue" @keyup.enter.native="search"
+                                      placeholder="Keywords search (Enter)"/>
+                        </el-col>
+                        <el-col :span="11">
+                            <div class="my-button">
+                                <el-button plain style="width: 100%;" color="#2fa7b9" @click="addTeacher">Add teacher</el-button>
+                            </div>
+                        </el-col>
+
+                        <el-col :span="3" style="display: inline-flex;justify-content: center;align-items: center; cursor: pointer;">
+                            <el-icon style="font-size: 20px;color: #b3b6bc;" @click="refresh">
+                                <Refresh />
+                            </el-icon>
+                        </el-col>
+                    </el-row>
+                </div>
+                <!--Search area end-->
+
+            </div>
+        </template>
+        <!--Header end-->
+</template>
+
+<style scoped>
+
+</style>
