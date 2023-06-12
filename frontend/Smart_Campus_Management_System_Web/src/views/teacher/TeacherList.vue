5cb1272499ea0883f3cca769322bdb6cecf04957
LeahFu <leahfu818@gmail.com>
2023-06-12 15:20:44 -0400

    teacher management page - tablebox area layout2

--- frontend/Smart_Campus_Management_System_Web/src/views/teacher/TeacherList.vue
+++ frontend/Smart_Campus_Management_System_Web/src/views/teacher/TeacherList.vue
@@ -32,10 +32,76 @@
                     </el-row>
                 </div>
                 <!--Search area end-->
-
             </div>
         </template>
         <!--Header end-->
+
+        <!--Table-box area start-->
+        <div class="table-box">
+            <el-table element-loading-text="Loading..." v-loading="loading" :data="tableData"
+                      style="width: 100%;text-align: center" :cell-style="{textAlign: 'center'}"
+                      :row-class-name="rowClassName"
+                      :header-cell-style="{fontSize: '15px', background: '#178557',color: 'white',textAlign: 'center'}">
+
+                <el-table-column label="Serial number" width="100" type="index" :index="Nindex"/>
+                <el-table-column label="Teacher ID">
+                    <template #default="scope">
+                        <span>{{scope.row.teachno}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Teacher name">
+                    <template #default="scope">
+                        <span>{{scope.row.name}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Course name">
+                    <template #default="scope">
+                        <span>{{scope.row.course.coursename}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Gender">
+                    <template #default="scope">
+                        <span>{{scope.row.gender}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Phone">
+                    <template #default="scope">
+                        <span>{{scope.row.phone}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Email">
+                    <template #default="scope">
+                        <span>{{scope.row.email}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Created time">
+                    <template #default="scope">
+                            <span>{{scope.row.createTime}}</span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="Operate">
+                    <template #default="scope">
+                        <el-button size="small" @click="editTeacher(scope.row.id)"
+                                   style="margin: 0 0 10px 10px;">Edit</el-button>
+                        <el-popconfirm confirm-button-text="Submit" cancel-button-text="Cancel" :icon="Delete"
+                                       icon-color="#626AEF" :title="'Are you sure you want to delete “'+scope.row.name+'” ?'"
+                                       @confirm="delTeacher(scope.row.id)">
+                            <template #reference>
+                                <el-button size="small" type="danger" style="margin-bottom: 10px;">Delete</el-button>
+                            </template>
+                        </el-popconfirm>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
+        <!--Table-box area end-->
 </template>
 
 <style scoped>
