cf826a19036972ce232618dcd2666b3002755f74
LeahFu <leahfu818@gmail.com>
2023-06-20 15:20:18 -0400

    implement grades getList

--- backend/Smart_Campus_Management_System/src/main/java/ca/access/student/service/IScoresService.java
+++ backend/Smart_Campus_Management_System/src/main/java/ca/access/student/service/IScoresService.java
@@ -1,9 +1,19 @@
 package ca.access.student.service;
 
+import ca.access.student.service.dto.ScoresQueryCriteria;
+import org.springframework.data.domain.Pageable;
+
 /**
  * @author: Lei Fu
  * @date: 2023/06/17
  * @description: Grade management business interface
  */
 public interface IScoresService {
+    /**
+     * Get grades list
+     * @param queryCriteria
+     * @param pageable
+     * @return
+     */
+    Object getList(ScoresQueryCriteria queryCriteria, Pageable pageable);
 }
--- backend/Smart_Campus_Management_System/src/main/java/ca/access/student/service/impl/ScoresServiceImpl.java
+++ backend/Smart_Campus_Management_System/src/main/java/ca/access/student/service/impl/ScoresServiceImpl.java
@@ -1,7 +1,13 @@
 package ca.access.student.service.impl;
 
+import ca.access.student.domain.Scores;
 import ca.access.student.repository.ScoresRepository;
 import ca.access.student.service.IScoresService;
+import ca.access.student.service.dto.ScoresQueryCriteria;
+import ca.access.utils.PageUtil;
+import ca.access.utils.QueryHelp;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,5 +22,16 @@ public class ScoresServiceImpl implements IScoresService {
     private final ScoresRepository scoresRepository;
     public ScoresServiceImpl(ScoresRepository scoresRepository) {
         this.scoresRepository = scoresRepository;
+    }
+    /**
+     * Get grades list
+     * @param queryCriteria
+     * @param pageable
+     * @return
+     */
+    @Override
+    public Object getList(ScoresQueryCriteria queryCriteria, Pageable pageable) {
+        Page<Scores> page = scoresRepository.findAll((root, query, criteriaBuilder) -> QueryHelp.getPredicate(root,queryCriteria,criteriaBuilder),pageable);
+        return PageUtil.toPage(page);
     }
 }
