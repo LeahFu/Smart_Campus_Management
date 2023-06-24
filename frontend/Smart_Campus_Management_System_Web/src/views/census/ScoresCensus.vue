845cfede8e3520947c8d536598e4550dedc70406
LeahFu <leahfu818@gmail.com>
2023-06-23 22:00:42 -0400

    grades statistics page layout

---
+++ frontend/Smart_Campus_Management_System_Web/src/views/census/ScoresCensus.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <el-card class="box-card">
+        <!--Header start-->
+        <template #header>
+            <div class="card-header">
+                <h3>
+                    <el-icon style="margin-right: 10px;"><Histogram /></el-icon>Class subject performance statistics
+                </h3>
+                <!--Search area start-->
+                <div class="card-search">
+                    <el-row :gutter="8">
+                        <el-col :span="12">
+                            <el-select v-model="gradeClassId" placeholder="Please select a class" style="width: 100%;" >
+                                <el-option v-for="item in gradeClassOptions" :key="item.id" :label="item.name" :value="item.id" />
+                            </el-select>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-select v-model="courseId" placeholder="Please select a subject" style="width: 100%;" >
+                                <el-option v-for="item in courseOptions" :key="item.id" :label="item.name" :value="item.id" />
+                            </el-select>
+                        </el-col>
+                    </el-row>
+                </div>
+                <!--Search area end-->
+            </div>
+        </template>
+        <!--Header end-->
+    </el-card>
+</template>
+
+<style scoped>
+
+</style>
