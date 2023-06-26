b778d4accd5d4aa99955b290624506a2c0b098f3
LeahFu <leahfu818@gmail.com>
2023-06-26 14:42:50 -0400

    create ScoreContrastCensusBar-1

---
+++ frontend/Smart_Campus_Management_System_Web/src/views/census/components/ScoreContrastCensusBar.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import * as echarts from 'echarts'
+import { EChartsType } from 'echarts/core'
+import { onMounted } from 'vue'
+
+let props = defineProps({
+    className: {
+        type:String,
+        default: 'chart'
+    },
+    id: {
+        type:String,
+        default: 'chart'
+    },
+    height: {
+        type:String,
+        default: '200px'
+    },
+    width: {
+        type: String,
+        default: '200px',
+    }
+})
+const options = {
+    grid: {
+        top:10,
+        left: '2%',
+        right: '2%',
+        bottom: '2%',
+        containLabel: true,
+    },
+    xAxis: {
+        type: 'category',
+        data: []
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: []
+}
+let chart: EChartsType
+const initChart = () => {
+    let chart = echarts.init(document.getElementById(props.id))
+    chart.setOption(options)
+    return chart
+}
+onMounted(() => {
+    chart = initChart()
+    window.addEventListener('resize', function () {
+        chart && chart.resize()
+    })
+})
+</script>
+
+<template>
+    <div :id="id" :class="className" :style="{ height: height, width: width }" />
+</template>
+
+<style scoped>
+
+</style>
