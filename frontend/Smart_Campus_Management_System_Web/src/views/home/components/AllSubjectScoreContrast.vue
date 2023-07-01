51cb4abb91047036bf0c49cb5dd5b3494d85c484
LeahFu <leahfu818@gmail.com>
2023-07-01 18:06:23 -0400

    course score comparison - front end-1

---
+++ frontend/Smart_Campus_Management_System_Web/src/views/home/components/AllSubjectScoreContrast.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import * as echarts from 'echarts'
+import { EChartsType } from 'echarts/core'
+import { ref, onMounted} from 'vue'
+
+let props = defineProps({
+    legendData: {
+        type:Array,
+        default: []
+    },
+    categoryData: {
+        type:Array,
+        default: []
+    },
+    seriesData: {
+        type:Array,
+        default: []
+    },
+    className: {
+        type: String,
+        default: 'chart',
+    },
+    id: {
+        type: String,
+        default: 'chart',
+    },
+    width: {
+        type: String,
+        default: '200px',
+    },
+    height: {
+        type: String,
+        default: '200px',
+    },
+})
+</script>
+
+<template>
+    <div :id="id" :class="className" :style="{ height: height, width: width }" />
+</template>
+
+<style scoped>
+
+</style>
