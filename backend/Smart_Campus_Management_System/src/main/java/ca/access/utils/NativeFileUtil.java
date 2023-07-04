cff70341093432a859f7df9b122a2fa2d04f7dfd
LeahFu <leahfu818@gmail.com>
2023-07-03 20:11:30 -0400

    implement uploadUserIcon-back end -1

---
+++ backend/Smart_Campus_Management_System/src/main/java/ca/access/utils/NativeFileUtil.java
@@ -0,0 +1,47 @@
+package ca.access.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author: Lei Fu
+ * @date: 2023/07/03
+ * @description: Upload local files
+ */
+public class NativeFileUtil {
+    /**
+     * @param uploadFile
+     * @param filePath
+     * @return
+     */
+    public static String uploadUserIcon(MultipartFile uploadFile, String filePath){
+
+        // Get original name
+        String oldName = uploadFile.getOriginalFilename();
+        // Get file size
+        long pictureSize = uploadFile.getSize();
+        // File extension
+        String fileExtension = oldName.substring(oldName.lastIndexOf("."), oldName.length());
+        if(fileExtension.equals(".png")||fileExtension.equals(".jpg")||fileExtension.equals(".gif")){
+            File folder = new File(filePath);
+            if(!folder.isDirectory()) {
+                folder.mkdirs();
+            }
+            String newName = UUID.randomUUID().toString() +
+                    oldName.substring(oldName.lastIndexOf("."), oldName.length());
+            // File save operation
+            try {
+                uploadFile.transferTo(new File(folder, newName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return newName;
+        }else{
+            return null;
+        }
+    }
+}
+}
